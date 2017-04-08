2821d70231df4b652ec1d33c63ed0a2635636803
mregueira <mregueira@itba.edu.ar>
2017-04-07 23:25:37 -0300

    agregados comentarios y define para enter

--- TP4/tp_final.c
+++ TP4/tp_final.c
@@ -13,11 +13,13 @@
 #define MAX_NB 3
 #define ALIVE '*' // Simbologia adoptada para identificar celulas vivas y muertas
 #define DEAD ' '
+#define IS_ENTER 1
+#define NOT_ENTER 0
 
 #define CNT_NBOURS 8 // Cantidad de vecinos de una celda (usada en una matriz para el funcionamiento del juego)
 
 
-#define INPUT_ERR -1 // Codigos de error input
+#define INPUT_ERR -1 // Codigos de error de input de teclado
 #define INPUT_ENTER -2
 
 // Criterio: la matrix hardcodeada se declara global. De esta forma
@@ -32,21 +34,33 @@ char seed_matrix[M][N] = {
 	{' ',' ','*','*',' ',' '}
 };
 
+/* init_game: inicializa el juego, recibe la matriz local a utilizar, y el contenido
+   de la matriz semilla (seed_matrix) es copiado a la matriz pasada de referencia. */
 void init_game(char mat[][N]);
 
-void finalize_evolution(char mat[][N]); // pasar al siguiente estado
+/* finalize_evolution: recibe la referncia a la matriz del juego, y modifica una 
+   matriz local con los nuevos estados. Al finalizar copia la matriz resultante 
+   a la del juego. */
+void finalize_evolution(char mat[][N]);
 
+/* output_world: recibe la referncia a la matriz del juego, y muestra en pantalla
+   el ultimo estado modificado. */
 void output_world(char mat[][N]);
 
+/* copy_matrix: recibe una referencia a una matriz origen (src) y a una matriz 
+   destino (dst), y copia el contenido de la matriz origen en la matriz de destino*/
+void copy_matrix (char src[][M], char dst[][M]); 
 
-//// funciones mas auxiliares 
-void copy_matrix (char src[][M], char dst[][M]); // copia de src(source) a dst(destination)
+/* next_cell_ev: recibe las coordenadas de una celda, y la matriz de juego, y devuelve
+   el estado que tendra dicha celda en la siguiente iteracion. */
+int next_cell_ev(int i,int j,char mat[][M]); 
 
-int next_cell_ev(int i,int j,char mat[][M]); // si estamos en la iteracion i, nos retornara el estado de la celda en la iteracion i+1
+/* cnt_nbours: recibe las coordenadas de una celda, y la matriz de juego, y devuelve la
+   cantidad de celulas vecinas vivas que posee en las 8 celdas de alrededor. */
+int cnt_nbours(int i, int j,char mat[][N]);
 
-int cnt_nbours(int i, int j,char mat[][N]); // cotar la cantidad de vecinos vivos de una celda
-
-int leer_numero(); // leer un entero de la entrada estandar
+/* leer_numero: leer un entero de la entrada estandar */
+int leer_numero(); 
 
 int main(){
 	char game_matrix[M][N];
@@ -73,7 +87,7 @@ int main(){
 			printf("Entrada invalida \n");
 		}else if (it != 0){
 			int j;
-			for (j = 0;j < it;j++){ // iteramos it veces el mundo
+			for (j = 0;j < it;j++){ // iteramos "it" veces el mundo
 				i ++ ;
 				finalize_evolution(game_matrix); 
 				if (j != it-1){ // no queremos ver la misma matriz dos veces!
@@ -87,16 +101,16 @@ int main(){
 	}
 }
 
-void finalize_evolution(char mat[][N]){ // actualizamos cada casillero por separado
+void finalize_evolution(char mat[][N]){ // Actualizamos cada casillero por separado
 	int i , j; 
-	char aux_next[N][M]; // matriz auxiliar con el futuro estado del juego.
+	char aux_next[N][M]; // Matriz auxiliar con el futuro estado del juego.
 
 	for (i = 0;i < M;i++){
 		for (j = 0;j < N;j++){
-			aux_next[i][j] = next_cell_ev(i,j,mat); // estudiamos el estado de la celda segun las reglas
+			aux_next[i][j] = next_cell_ev(i,j,mat); // Estudiamos el estado de la celda segun las reglas
 		}
 	}
-	copy_matrix(aux_next , mat); // ahora si se actualiza la matriz del juego
+	copy_matrix(aux_next , mat); // Ahora si se actualiza la matriz del juego
 }
 
 int next_cell_ev (int i,int j,char mat[][M]){
@@ -108,14 +122,14 @@ int next_cell_ev (int i,int j,char mat[][M]){
 	cant_vecinos = cnt_nbours(i, j, mat);
 
 	switch(cell_status){
-		case ALIVE: // If the cell is ALIVE
+		case ALIVE: // Si la celula esta "ALIVE"
 			if((cant_vecinos < MIN_NB) || (cant_vecinos > MAX_NB)){
 				new_state = DEAD;
 			}else if((cant_vecinos == MIN_NB) || (cant_vecinos == MAX_NB)){
 				new_state = ALIVE;
 			}
 		break;
-		case DEAD: // If the cell is DEAD
+		case DEAD: // Si la celula esta "DEAD"
 			if(cant_vecinos == MAX_NB){
 				new_state = ALIVE;
 			}else{
@@ -131,7 +145,7 @@ int cnt_nbours(int i, int j,char mat[][N]){
     int ans = 0;
 
 	int mv_i[CNT_NBOURS] = {1, 1, 0,-1,-1,-1,0,1};
-	int mv_j[CNT_NBOURS] = {0,-1,-1,-1 ,0, 1,1,1}; // OJO
+	int mv_j[CNT_NBOURS] = {0,-1,-1,-1 ,0, 1,1,1}; 
 
     for(k = 0 ;k < CNT_NBOURS;k++){
 		int n_i = i + mv_i[k];
@@ -161,9 +175,9 @@ void init_game(char mat[][N]){
 }
 
 int leer_numero(){
-	int ans = 0; // en el caso de que el usuario solo presione enter
+	int ans = 0; // En el caso de que el usuario solo presione enter
 	int abort = 0;
-	int f_enter = 1; // flag para controlar si el usuario solo escribe enter
+	int f_enter = IS_ENTER; // Flag para controlar si el usuario solo escribe enter
 
 	char c;
 	while ( (c = getchar()) != '\n'){
@@ -171,14 +185,14 @@ int leer_numero(){
 			ans *= 10;
 			ans += (c - '0');
 		}else{
-			abort = 1; // seguiremos iterando para vaciar el buffer
+			abort = 1; // Seguiremos iterando para vaciar el buffer
 		}
-		f_enter = 0;
+		f_enter = NOT_ENTER;
 	} 
 	if (abort){
 		ans = INPUT_ERR;
 	}
-	if (f_enter){
+	if (f_enter == IS_ENTER){
 		ans = INPUT_ENTER;
 	}
 	return ans;
