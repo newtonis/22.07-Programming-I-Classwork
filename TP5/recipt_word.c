22ca26bc5f35a2e522e870ceb4ea0a7d97661d07
newtonis <newtonis.penguin@gmail.com>
2017-04-15 18:52:12 -0300

    6:18

--- TP5/recipt_word.c
+++ TP5/recipt_word.c
@@ -2,31 +2,20 @@
 
 #define WAIT_WORD -3
 #define WORD_OK -2
-#define ERR_CHAR -1 
 #define START 0
 #define WAIT 1
 #define INFO_OK 2
 #define IS_ENTER 3
 #define WORD_LIMIT 20
-#define C_LET (26*2)
-#define C_ASCI 256
 
+#define CNT_LET (26*2)
+#define CNT_ASCI 256
 
+enum {ERR_SZ,ERR_CHAR_VALID,VALID};
 enum {FALSE , TRUE};
 
-// sets all array elements to 0
-void init_null_arr(char arr[], int sz);
-
-// reads from input one word. sz=0 if no word entered
-void recipt_word (char str[], int *sz);
-
-void welcome_print(void);
-
-// checks a word to be valid, if all chars are valid letters
-void valid_word(char word[],int word_sz);
-
 // The array valid_let is global because its for read only purposes
-char valid_let[C_LET] = 
+char valid_let[CNT_LET] = 
 {'a','b','c','d','e',
 'f','g','h','i','j',
 'k','l','m','n','o',
@@ -38,40 +27,55 @@ char valid_let[C_LET] =
 'P','Q','R','S','T',
 'U','V','W','X','Y','Z'};
 
-int main()
-{
-	enum{W1 = 1, W2};
 
-	int state = WAIT_WORD;
+// sets all array elements to 0
+void init_null_arr(int arr[], int sz);
+// reads from input one word. sz=0 if no word entered
+void recipt_word (char str[], int *sz);
+void welcome_print();
+// checks a word to be valid, if all chars are valid letters
+int valid_word(char word[], int word_sz , int is_valid[]);
 
-	char word_1[WORD_LIMIT], w1_chars[C_ASCI]; 
-	//char word_2[WORD_LIMIT], w2_chars[C_ASCI];
+// convert letter list to asci table (valid letters value is 1)
+void process_valid(int is_valid[] ,char let_list[] , int let_list_sz);
 
-	init_null_arr(w1_chars, C_ASCI);
+int main(){
+	// compute valid letters table
+	int is_valid[CNT_ASCI];
+	process_valid(is_valid , valid_let , CNT_ASCI);
+
+	char str1[WORD_LIMIT] , str2[WORD_LIMIT];
+	int sz1, sz2;
 
 	welcome_print();
+	printf("Insert first word: ");
+	recipt_word(str1,&sz1);
+	printf("Insert second word: ");
+	recipt_word(str2,&sz2);
+	
+	int valid1 = valid_word(str1 , sz1 , is_valid);
+	int valid2 = valid_word(str2 , sz2 , is_valid);
 
-	while(state == WAIT_WORD){
-		recipt_word(word_1, w1_chars, W1);
-		//validate_word(word_1, &state);
-		if(state == ERR_CHAR){
-			printf("Word %d entered wrong. Try again: ", W1);
-			state = WAIT_WORD;
-		}
-	}
-
+	printf("%d %d",valid1,valid2);
 	return 0;
 }
 
-void welcome_print(void){
+void welcome_print(){
 	printf("Welcome to the Anagram Search Program\n");
 	printf("-------------------------------------\n\n");
-	printf("Enter words in spanish with a length of no more than %d letters.\n", WORD_LIMIT); 
-	printf("DO NOT include numbers or strange symbols.\n");
+	printf("Max word length: %d\n", WORD_LIMIT); 
+	printf("Valid characters: ");
+	int i;
+	for (i = 0;i < CNT_LET;i++){
+		printf("%c ",valid_let[i]);
+	}
+	printf("\n");
+	printf("-------------------------------------\n\n");
 }
 
 void recipt_word (char str[],int *sz){
 	int i = 0;
+	char aux_var;
 	while((aux_var = getchar()) != '\n'){
 		str[i] = aux_var;
 		i++;
@@ -79,26 +83,31 @@ void recipt_word (char str[],int *sz){
 	*sz = i;
 }
 
-void process_valid(int is_valid[]){
-	init_null_arr(is_valid , str);
+void process_valid(int is_valid[] ,char let_list[] , int let_list_sz){
+	init_null_arr(is_valid , let_list_sz);
 	int i = 0;
-	for (i = 0;i < C_LET;i++){
-		is_valid[ valid_let[i] ] = TRUE;
+	for (i = 0;i < let_list_sz;i++){
+		is_valid[ (int)let_list[i] ] = TRUE;
 	}
 }
 
 int valid_word(char word[], int word_sz , int is_valid[]){
-	int i;
-	ans = FALSE;
-	for(i = 0;!ans && i < word_sz;i++){
-		if (is_valid[word[i]]){
-			ans = TRUE;
+	int ans;
+	if (word_sz > WORD_LIMIT){
+		ans = ERR_SZ;
+	}else{
+		int i;
+		ans = ERR_CHAR_VALID;
+		for(i = 0;(ans!=VALID) && i < word_sz;i++){
+			if (is_valid[ (int)word[i] ]){
+				ans = VALID;
+			}
 		}
 	}
 	return ans;	
 }
 
-void init_null_arr(char arr[], int sz){
+void init_null_arr(int arr[], int sz){
 	int i;
 	for(i = 0; i < sz; i++){
 		arr[i] = 0;
