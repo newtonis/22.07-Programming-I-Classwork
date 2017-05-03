ace7deff9fdd1af58a72f3b66129f944f81ad40c
joa <jmestanza@itba.edu.ar>
2017-05-03 20:38:37 -0300

    added calc_res

--- TP6_REAL/actions.c
+++ TP6_REAL/actions.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #define ERR -1
+#define NO_ERR 0
 
 float calc_res(float x, float y, char op);
 float (∗actions[MAX_OPERATORS]) (float ,float) = {add,sub,mult};
 int main(){
 
+
+
 }
 
-float calc_res(float x, float y, char op){
-	float *p2x=&x;
-	float *p2y=&y;
-	
+float calc_res(float x, float y, char op,int *p2err){
 
+	int err_op=NO_ERR;
+	int *p2err=&err_op;
+
+		if( ( *(operators[op]) ) != ERR ){	
+			ans=(actions[operators[op]])(x,y);
+		
+	}else{
+		*(p2err)=ERR;
+	}
+	return ans;
 }
 
 float add(float a,float b){
