5e395a835a196a5b9aae959bd4f10b715ef04207
joa <jmestanza@itba.edu.ar>
2017-05-06 18:59:49 -0300

    fusione la mia y la de marce, compila y parece funcionar

--- TP6_REAL/actions.c
+++ TP6_REAL/actions.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #define ERR -1
 #define NO_ERR 0
+#define MAX_OPERATORS 10
+#define ASCI 256
+#define SYMBOL_MODE 2
+#define LETTER_MODE 3
 
 float calc_res(float x, float y, char op);
 float add(float a,float b);
@@ -8,21 +12,36 @@ float sub(float a,float b);
 float mult(float a,float b);
 float div(float a,float b);
 
+void set_operations_symbol(int mode); 
 
-float (∗actions[MAX_OPERATORS]) (float ,float) = {add,sub,mult,div};
+void add_operation(char o, float (*a)(float, float));
+
+//// GLOBAL VARIABLES ////
+int op_asign; // Only used by set_operations_symbol
+int operators[ASCI];
+float (* actions [MAX_OPERATORS])(float, float);
+//// ---------------- ////
+int err_op=NO_ERR;
+int *p2err=&err_op;
+	
 
 int main(){
+	int op_mode = SYMBOL_MODE;
+	set_operations_symbol(op_mode);
 	
+	float x =0.2;
+	float y =0.0;
+	char op = '/';
+	printf("%lf\n",calc_res(x,y,op) );	
 
 }
 
 float calc_res(float x, float y, char op){
 
-	int err_op=NO_ERR;
-	int *p2err=&err_op;
+	float ans;
 
-		if( ( *(operators[op]) ) != ERR ){	
-			ans=(actions[operators[op]])(x,y);
+		if( ( operators[(int)op] != ERR )){	
+			ans=(actions[operators[(int)op]])(x,y);
 		
 	}else{
 		*p2err=ERR;
@@ -40,7 +59,7 @@ float sub(float a,float b){
 	ans= a-b;
 	return ans;
 }
-float mult(float a,float b){
+float mul(float a,float b){
 	float ans;
 	ans= a*b;
 	return ans;
@@ -48,14 +67,43 @@ float mult(float a,float b){
 
 float div(float a,float b){
 	float ans;
+
 	if(b!=0){
 	ans= a/b;
 	}else{
 		ans=ERR;
 		*p2err=ERR;
 	}
-	return ans;
-	
+	return ans;	
 }
 
+
+
+void set_operations_symbol(int mode){
+
+	op_asign = 0;
+
+	switch(mode){
+		case SYMBOL_MODE:
+			add_operation('+', add); // add function
+			add_operation('-', sub); // sub function 
+			add_operation('*', mul); // mul function
+			add_operation('/', div);
+		break;
+		case LETTER_MODE:
+			add_operation('a', add); // add function
+			add_operation('s', sub); // sub function 
+			add_operation('m', mul); // mul function
+			add_operation('d', div);
+		break;
+	}
+}
+
+void add_operation(char o, float (*a)(float, float)){
+
+	operators[(int)o] = op_asign;
+	actions[op_asign] = a;
+
+	op_asign++;
+}
 
