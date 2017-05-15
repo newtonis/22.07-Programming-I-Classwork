5e96139386d41097498ebcb928c6ed9680277546
joa <jmestanza@itba.edu.ar>
2017-05-14 22:48:46 -0300

    22:48

--- TP7/version_nueva/io.h
+++ TP7/version_nueva/io.h
@@ -1,6 +1,9 @@
 #ifndef IO_H
 #define IO_H
 
+#define INPUT_ERR -1 // Codigos de error de input de teclado
+#define INPUT_ENTER -2
+
 // leer un numero de la entrada de usuario
 int leer_numero();
 
