538254ef226ef6e7f51a23db67c23791feff1a98
newtonis <newtonis.penguin@gmail.com>
2017-05-14 22:53:08 -0300

    5-14, 10:37

--- TP7/version_nueva/game.h
+++ TP7/version_nueva/game.h
@@ -11,17 +11,33 @@
 
 #define CNT_NBOURS 8 // Cantidad de vecinos de una celda (usada en una matriz para el funcionamiento del juego)
 
+// Criterio: la matrix hardcodeada se declara global. De esta forma
+// esta cerca del principio del programa y es facil modificarla rapido
+
+char seed_matrix[M][N] = {
+	{' ',' ',' ',' ',' ',' '},
+	{' ','*','*','*','*',' '},
+	{' ','*','*','*',' ',' '},
+	{' ','*','*','*','*',' '},
+	{' ','*',' ',' ',' ',' '},
+	{' ',' ','*','*',' ',' '}
+};
 
 void init_game(char mat[][N]);
 
 void get_world_matrix(char mat[][N]);
 
-void finalize_evolution(char mat[][N]);
+void next_evolution(char mat[][N]);
 
 
 /* next_cell_ev: recibe las coordenadas de una celda, y la matriz de juego, y devuelve
    el estado que tendra dicha celda en la siguiente iteracion. */
 static int next_cell_ev(int i,int j,char mat[][M]); 
+
+/* cnt_nbours: recibe las coordenadas de una celda, y la matriz de juego, y devuelve la
+   cantidad de celulas vecinas vivas que posee en las 8 celdas de alrededor. */
+static int cnt_nbours(int i, int j,char mat[][N]);
+
 
 
 
