cf77b6d03b33dd98a9f26ce62b3ef4c05a7730cf
newtonis <newtonis.penguin@gmail.com>
2017-05-14 23:42:14 -0300

    5-14, 10:45

--- TP7/version_nueva/front_end.h
+++ TP7/version_nueva/front_end.h
@@ -4,5 +4,7 @@
 void welcome_msg();
 void show_game_status(int i);
 int get_turn_user_input();
+void show_error();
+void show_matrix(char mat[][N]);
 
 #endif FRONT_END_H //FRONT_END_H
--- TP7/version_nueva/utils.c
+++ TP7/version_nueva/utils.c
@@ -1,4 +1,5 @@
 #include "game.h"
+
 void copy_matrix (char src[][N], char dst[][N]){
 	int i, j;
 	for(i = 0; i < M; i++){
@@ -8,14 +9,3 @@ void copy_matrix (char src[][N], char dst[][N]){
 	}
 }
 
-void output_world(char mat[][N]){
-	int i, j;
-	printf("\n");
-	for (i = 0;i < N;i++){
-		for (j = 0;j < M;j++){
-			printf("|%c",mat[i][j]);
-		}
-		printf("|\n");
-	}
-	printf ("\n");
-}
