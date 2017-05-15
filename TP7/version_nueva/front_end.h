b29cb4affc90cfbde06c50b14830c320a419fa3f
newtonis <newtonis.penguin@gmail.com>
2017-05-14 23:06:30 -0300

    5-14, 10:37

--- TP7/version_nueva/front_end.h
+++ TP7/version_nueva/front_end.h
@@ -1,20 +1,6 @@
 #ifndef FRONT_END_H
 #define FRONT_END_H
 
-// Funciones relacionadas con la interaccion con el usuario
 
 
-// mensaje de bienvenida
-void welcome_msg(); 
-
-// mensaje de turno de juego
-void game_msg();
-
-/* recibe la referncia a la matriz del juego, y muestra en pantalla
-   el ultimo estado modificado. */
-void output_world(char mat[][N]);
-
-
-
-
-#endif // FRONT_END_H
+#endif FRONT_END_H //FRONT_END_H
