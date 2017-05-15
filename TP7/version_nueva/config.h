0066992557797e8e37bdc3ed2394a11d3976934e
newtonis <newtonis.penguin@gmail.com>
2017-05-14 23:57:29 -0300

    5-14, 10:45

---
+++ TP7/version_nueva/config.h
@@ -0,0 +1,27 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Todas las definiciones relacionadas
+// con el funcionamiento del juego
+
+
+#define M 6 // Cantidad de filas
+#define N 6 // Cantidad de columnas
+#define MIN_NB 2 // Configuracion minima y maxima de la cantidad de vecinos para una celda ser viva o muerta
+#define MAX_NB 3
+#define ALIVE '*' // Simbologia adoptada para identificar celulas vivas y muertas
+#define DEAD ' '
+
+#define CNT_NBOURS 8 // Cantidad de vecinos de una celda (usada en una matriz para el funcionamiento del juego)
+
+// Criterio: la matrix hardcodeada se declara global. De esta forma
+// esta cerca del principio del programa y es facil modificarla rapido
+
+char seed_matrix[M][N] = {
+	{' ',' ',' ',' ',' ',' '},
+	{' ','*','*','*','*',' '},
+	{' ','*','*','*',' ',' '},
+	{' ','*','*','*','*',' '},
+	{' ','*',' ',' ',' ',' '},
+	{' ',' ','*','*',' ',' '}
+};
