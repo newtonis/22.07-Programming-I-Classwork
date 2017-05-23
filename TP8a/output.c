246fcfdca46becfa7edfca75a93e38876b6ecbb0
joa <jmestanza@itba.edu.ar>
2017-05-23 01:06:08 -0300

    1:06

--- TP8a/output.c
+++ TP8a/output.c
@@ -14,3 +14,12 @@ void showPort(port_t *port){
 	}
 	printf("\n");
 }
+
+void welcome(void){
+	printf("%s\n", "Welcome to the LED simulator program'\n'" );
+	printf("%s\n", "-Please enter: ");
+	printf("%s\n", "- a number from 0 to 7 if you want an specific LED to be on");
+	printf("%s\n", "- 'b' if you want all the LEDS that are on to be triggered" );
+	printf("%s\n", "- 'c' if you want all the LEDS off" );
+	printf("%s\n", "- 's' if you want all the LEDS on" );
+}
