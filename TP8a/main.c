48bc491c0d134391f096e9db4e39d8f96a4b894e
joa <jmestanza@itba.edu.ar>
2017-05-22 23:43:46 -0300

    22:43

--- TP8a/main.c
+++ TP8a/main.c
@@ -2,61 +2,47 @@
 #include "port_utils.h"
 #include "output.h"
 
+#define ESCAPE (27)
+
 int main(){
 	microPorts_t mp;
 
 	int ans = initPorts(&mp);
 
-	bitSet(&mp.A,7);
-
-	showPort(&mp.A);
-	showPort(&mp.B);
-	showPort(&mp.D);
-
-	maskOn(&mp.A,0xFF);
-
-	showPort(&mp.A);
-	showPort(&mp.B);
-	showPort(&mp.D);
+	int end =0;
+	char c;
+	c=getchar();
+	while(!end){
 
 
-
-	bitClr(&mp.A,0);
-	showPort(&mp.A);
-	showPort(&mp.B);
-	showPort(&mp.D);
-
-	maskOff(&mp.A,0xFF);
-	showPort(&mp.A);
-	showPort(&mp.B);
-	showPort(&mp.D);
-
-<<<<<<< HEAD
-	maskToggle(&mp.A,0x1);
-=======
-	bitSet(&mp.A,1);
-	bitSet(&mp.B,1);
-	bitSet(&mp.B,);
-	maskOff(&mp.D,0xFFF);
->>>>>>> 25eb6766ac95cfeed3f5ebba02c5be664bc94083
-
-	showPort(&mp.A);
-	showPort(&mp.B);
-	showPort(&mp.D);
-
-	printf("%d\n",bitGet(&mp.A,0x0));
-
+		if(('0'<=c)&&(c<='7')){
+			bitSet(&mp.A,(int)(c-'0'));
+			showPort(&mp.A);
+		}else if(c=='b'){
+			int i=0;
+			int mask_aux=0;
+			for(int i;i<8;i++){
+				if(bitGet(&mp.A,i)==1){
+					mask_aux |=(1<<(7-i)); 
+											// 01110000
+				}						   // 
+			}
+			printf("%d",mask_aux);
+			maskToggle(&mp.A,mask_aux);
+			showPort(&mp.A);
+		}else if(c=='c'){
+			maskOff(&mp.A,0xFF);
+			showPort(&mp.A);
+		}else if(c=='s'){
+			maskOn(&mp.A,0xFF);
+			showPort(&mp.A);
+		}else if((int)c==ESCAPE){
+			end=0;
+		}
+		c=getchar();
+	}
 /*
-	maskToggle(&mp.A,0xFF);
-	maskToggle(&mp.B,0xFF);
-
-	show_port(&mp.A);
-	show_port(&mp.B);
-	show_port(&mp.D);
-
 */
 	endPorts(&mp); /// free ports memory
 	
-	
-	printf("%d ",ans);
 }
