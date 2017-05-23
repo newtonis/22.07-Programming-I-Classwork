cd34e318631d52d794e4940d40db33221f6db404
newtonis <newtonis.penguin@gmail.com>
2017-05-22 21:37:59 -0300

    5-22

--- TP8a/output.h
+++ TP8a/output.h
@@ -1,5 +1,21 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
+void show_port(port_t *port){
+	
+	printf("%c :",port->name);
+	if (port->dir != NULL){
+		int i;
+		uint16_t bit;
+		for (i = 0;i < port->sz;i++){
+			printf("%d",(*port->dir)&bit);
+			bit <<= 1;
+		}
+	}else{
+		printf("NULL");
+	}
+	printf("\n");
+}
+
 
 #endif // OUTPUT_H
