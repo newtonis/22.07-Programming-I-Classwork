9a1c307dd8e64cab56815c593727e2805dbf065f
mregueira <mregueira@itba.edu.ar>
2017-05-28 01:41:37 -0300

    changes

--- TP8b/graphic.c
+++ TP8b/graphic.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 #include "port_utils.h"
 #include "output.h"
+#include  "graphic.h"
 #include <unistd.h>
 
 void update_display(microPorts_t *mp,int *mode, ALLEGRO_BITMAP *led_ON, ALLEGRO_BITMAP *led_OFF){
+    
+    int src_ledON_W = al_get_bitmap_width(led_ON);
+    int src_ledON_H = al_get_bitmap_height(led_ON);
+    int src_ledOFF_W = al_get_bitmap_width(led_OFF);
+    int src_ledOFF_H = al_get_bitmap_height(led_ON);
+    
+    al_draw_scaled_bitmap(led_ON,0,0,src_ledON_W,src_ledON_H,X_LED_POS,Y_LED_POS,64,64,0);
+    al_flip_display();
 /*
 	int i;	
 	for (i = 0;i < mp->A.sz;i++){
--- TP8b/graphic.h
+++ TP8b/graphic.h
@@ -9,6 +9,9 @@
 
 #include "port_utils.h"
 
+#define X_LED_POS 20
+#define Y_LED_POS 250
+
 void update_display(microPorts_t *mp,int *mode, ALLEGRO_BITMAP *led_ON, ALLEGRO_BITMAP *led_OFF);
 
 #endif // GRAPHIC_H
