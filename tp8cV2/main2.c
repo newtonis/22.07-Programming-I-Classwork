77db73d4c7e683a7c2ee0dcdd4b57d0c4e6eb2e4
Joaquin <jmestanza@itba.edu.ar>
2017-06-06 20:33:09 -0300

    burocracia

--- tp8cV2/main2.c
+++ tp8cV2/main2.c
@@ -29,15 +29,16 @@
 //// --------------------------------------------------------------- ////
 /////////////////////////////////////////////////////////////////////////
 int main(){
+    char* led_codes[]={"4","17","27","22","18","23","24","25"}; 
+    
 	microPorts_t mp;
 	int err = initPorts(&mp);
-
 	if (err == 1){
 		printf("Fatal error, can't port heap memory");
 	}else{	
 		int end = 0;
 		int mode = NORMAL;
-
+               
 		nonblock(NB_ENABLE);
 		while (!end){
 			long tm = (long)time(NULL);
@@ -45,7 +46,9 @@ int main(){
 			userInput(&mp,&end,&mode,tm);
 			//while (tm == (long)time(NULL)); // wait to next second
 		}
-		nonblock(NB_DISABLE);
+		nonblock(NB_DISABLE); 
 		system("clear");
 	}
 }
+
+
