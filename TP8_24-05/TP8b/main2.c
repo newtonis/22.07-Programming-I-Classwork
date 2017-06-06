049d64501ab1724d70362f9eea4f9c56208fa6f9
mregueira <mregueira@itba.edu.ar>
2017-06-06 01:44:11 -0300

    new section

--- TP8_24-05/TP8b/main2.c
+++ TP8_24-05/TP8b/main2.c
@@ -11,7 +11,7 @@
 //// ---------------------------------------- ////
 //// ==> MESTANZA, Joaquin                    ////
 //// ==> NOWIK, Ariel                         ////
-//// ==> REGUEIRA, Marcelo   	  			  ///////////////////////////
+//// ==> REGUEIRA, Marcelo   	              ///////////////////////////
 //// --------------------------------------------------------------- ////
 //// The following code works as an emulator for leds connected to   ////
 //// A port. The status of each can be changed as shown in the       ////
@@ -19,15 +19,34 @@
 //// ==> output.h/output.c: functions to guide the user through the  ////
 ////     program.                                                    ////
 //// ==> port_utils.h/port_utils.c: internal functions for woking    ////
-////     with the ports.                             				 ////
+////     with the ports.                             		     ////
 //// ==> input.h/input.c: functions for update the standard out      ////
 ////     acording to user input.                                     ////
 //// ==> nonblock.h/nonblock.c: this library allows non blocking     ////
 ////     reading from stdin on Linux systems.                        ////
 //// ==> graphic.h/graphic.c: functions for update the standard out  ////
-////     with the new state of the leds.       						 ////
+////     with the new state of the leds.       			     ////
 //// --------------------------------------------------------------- ////
 /////////////////////////////////////////////////////////////////////////
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "config.h"
+#include "led_api.h"
+
+int main(int argc, char** argv) {
+    led_vars_t led_vars;
+    uint16_t led_manager;
+    int i = 0;
+    led_api_init(&led_vars , &led_manager , led_codes , CNT_LEDS);
+    led_manager = 0x01;
+    led_flush( &led_vars);
+    
+    //return (EXIT_SUCCESS);
+}
+*/
 int main(){
 	microPorts_t mp;
 	int err = initPorts(&mp);
