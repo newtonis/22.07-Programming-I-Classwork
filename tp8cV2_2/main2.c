9a4a267178af28bed754b4c6e6b4a4deab233a20
Joaquin <jmestanza@itba.edu.ar>
2017-06-06 22:42:09 -0300

    added comments

--- tp8cV2_2/main2.c
+++ tp8cV2_2/main2.c
@@ -9,29 +9,34 @@
 #include "output.h"
 #include "input.h"
 #include "config.h"
-//////////////////////////////////////////////////
-//// TP8b - STRUCT, UNION, BITFIELD - GROUP 3 ////
-//// ---------------------------------------- ////
-//// ==> MESTANZA, Joaquin                    ////
-//// ==> NOWIK, Ariel                         ////
-//// ==> REGUEIRA, Marcelo   	  			  ///////////////////////////
-//// --------------------------------------------------------------- ////
-//// The following code works as an emulator for leds connected to   ////
-//// A port. The status of each can be changed as shown in the       ////
-//// instructions when the program starts.                           ////
-//// ==> output.h/output.c: functions to guide the user through the  ////
-////     program.                                                    ////
-//// ==> port_utils.h/port_utils.c: internal functions for woking    ////
-////     with the ports.                             				 ////
-//// ==> input.h/input.c: functions for update the standard out      ////
-////     acording to user input.                                     ////
-//// ==> nonblock.h/nonblock.c: this library allows non blocking     ////
-////     reading from stdin on Linux systems.                        ////
-//// ==> graphic.h/graphic.c: functions for update the standard out  ////
-////     with the new state of the leds.       						 ////
-//// --------------------------------------------------------------- ////
-/////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+//// TP8c - Device drivers in user space - GROUP 3 ////
+//// ----------------------------------------------////
+//// ==> MESTANZA, Joaquin                         ////
+//// ==> NOWIK, Ariel                              ////
+//// ==> REGUEIRA, Marcelo   	                   /////////////////////////
+//// -------------------------------------------------------------------////
+//// Part of the following code works as an emulator for leds connected ////
+//// to A port. The status of each can be changed as shown in the       ////
+//// instructions when the program starts. Based on the led values      ////
+//// in port A, we turn on/off the leds connected in the RaspberryPi.   ////
+//// ==> output.h/output.c: functions to guide the user through the     ////
+////     program.                                                       ////
+//// ==> port_utils.h/port_utils.c: internal functions for working      ////
+////     with the ports.                             		        ////
+//// ==> input.h/input.c: functions for update the standard out         ////
+////     acording to user input.                                        ////
+//// ==> nonblock.h/nonblock.c: this library allows non blocking        ////
+////     reading from stdin on Linux systems.                           ////
+//// ==> graphic.h/graphic.c: functions for update the standard out     ////
+////     with the new state of the leds.                                ////
+//// ==>led_api.c/led_api.h : functions used for initializing and       ////
+////    modifying leds in the RaspberyPi                                ////
+//// -------------------------------------------------------------------////
+////////////////////////////////////////////////////////////////////////////
+
 int main(){
+    //indexed leds  e.g. led0 is 4, led1 is 17 and so on
     char* led_codes[]={"4","17","27","22","18","23","24","25"}; 
     
     microPorts_t mp;
@@ -48,7 +53,7 @@ int main(){
         int mode = NORMAL;
                
 	nonblock(NB_ENABLE);
-	while (!end){
+	while (!end){ // loop until Esc key is hit
             long tm = (long)time(NULL);
             update_display(&mp,&mode);
             userInput(&mp,&end,&mode,tm);
